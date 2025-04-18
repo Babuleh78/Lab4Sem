09d84e34473085f0a1477cc3346e534ed0dc610a
Babuleh78 <jdbdbsbbfdh@gmail.com>
2025-04-18 09:05:10 +0300

    hw

---
+++ hw.js
@@ -0,0 +1,17 @@
+function flatten(arr) {
+    let result = [];
+    
+    for (const item of arr) {
+      if (Array.isArray(item)) {
+        result.push(...flatten(item));
+      } else {
+        result.push(item);
+      }
+    }
+    
+    return result;
+  }
+
+const nestedArray = [1, 2, 3, [4, 5, 6, [10, 20, 30]]];
+const flatArray = flatten(nestedArray);
+console.log(flatArray); 
